95fd48f9cdbaf541911828be45bf1a81d2a689d9
Dirk S Beukes <dirk_beukes@hotmail.com>
2026-01-25 12:59:40 +0200

    Initial setup with shrd formula

---
+++ Formula/shrd.rb
@@ -0,0 +1,36 @@
+class Shrd < Formula
+  desc "Share anything instantly - CLI-first content sharing"
+  homepage "https://github.com/Stoffberg/shrd.sh"
+  version "0.1.0"
+  license "MIT"
+
+  on_macos do
+    on_intel do
+      url "https://github.com/Stoffberg/shrd.sh/releases/download/cli-v#{version}/shrd-x86_64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_DARWIN_X64"
+    end
+    on_arm do
+      url "https://github.com/Stoffberg/shrd.sh/releases/download/cli-v#{version}/shrd-aarch64-apple-darwin.tar.gz"
+      sha256 "PLACEHOLDER_DARWIN_ARM64"
+    end
+  end
+
+  on_linux do
+    on_intel do
+      url "https://github.com/Stoffberg/shrd.sh/releases/download/cli-v#{version}/shrd-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER_LINUX_X64"
+    end
+    on_arm do
+      url "https://github.com/Stoffberg/shrd.sh/releases/download/cli-v#{version}/shrd-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "PLACEHOLDER_LINUX_ARM64"
+    end
+  end
+
+  def install
+    bin.install "shrd"
+  end
+
+  test do
+    assert_match "shrd", shell_output("#{bin}/shrd --version")
+  end
+end
---
+++ README.md
@@ -0,0 +1,17 @@
+# Homebrew Tap
+
+Homebrew formulae for [shrd](https://github.com/Stoffberg/shrd.sh).
+
+## Installation
+
+```bash
+brew tap Stoffberg/tap
+brew install shrd
+```
+
+## Update
+
+```bash
+brew update
+brew upgrade shrd
+```
